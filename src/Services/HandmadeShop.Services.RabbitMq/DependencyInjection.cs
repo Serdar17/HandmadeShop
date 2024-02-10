92907a66b5bc2f243ac56f27e0ebf615d983bca4
serdar17 <useinovserdar23@gmail.com>
2024-02-10 15:49:31 +0500

    feat(broker): Added RabbitMq broker

---
+++ src/Services/HandmadeShop.Services.RabbitMq/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using HandmadeShop.Common.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetSchool.Services.RabbitMq;
+
+namespace HandmadeShop.Services.RabbitMq;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddRabbitMq(this IServiceCollection services, IConfiguration configuration = null)
+    {
+        var settings = Settings.Load<RabbitMqSettings>("RabbitMq", configuration);
+        services.AddSingleton(settings);
+
+        services.AddSingleton<IRabbitMq, RabbitMq.RabbitMq>();
+
+        return services;
+    }
+}
